c584abea7fe854ec6e4a45011e015f55592c5eb3
ASukhanov <sukhanov@bnl.gov>
2025-02-18 09:03:46 -0500

    Major refactoring

--- p2plant_ioc/__main__.py
+++ p2plant_ioc/__main__.py
@@ -1,8 +1,9 @@
 '''EPICS p4p-based softIocPVA for P2Plant devices
 '''
-__version__= 'v0.0.1 2025-02-14'# 
+__version__= 'v0.0.2 2025-02-18'# Major refactoring
+#TODO: handle multi-dimensional data
 
-import argparse, time, threading
+import time, threading
 import numpy as np
 import pprint
 from p4p.nt import NTScalar, NTEnum
@@ -10,35 +11,12 @@ from p4p.nt.enum import ntenum
 from p4p.server import Server
 from p4p.server.thread import SharedPV
 
-#`````````````````Constants```````````````````````````````````````````````````
-SleepTime = 1.
-EventExit = threading.Event()
-MaxPoints = 100
+from p2plantaccess import Access as PA
 
 #``````````````````Module properties``````````````````````````````````````````
-class G():
-    IocPrefix = None
-    PVs = {}
-    cycle = 0
-
-    def set_run(vntenum):
-        idx = vntenum.raw.value.index
-        #print(f">set_run {idx}")
-        if idx == 0:# Run
-            threading.Thread(target=G.threadProc).start()
-
-#``````````````````Argument parsing```````````````````````````````````````````
-parser = argparse.ArgumentParser(description = __doc__,
-  formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-  epilog=(f'{__version__}'))
-parser.add_argument('-l', '--listPVs', action='store_true', help=\
-'List all generated PVs')
-parser.add_argument('-p', '--prefix', default='p2p:', help=\
-'Prefix of all PVs')
-parser.add_argument('-v', '--verbose', action='count', default=0, help=\
-'Show more log messages (-vv: show even more)')
-pargs = parser.parse_args()
-P = pargs.prefix
+EventExit = threading.Event()
+pargs = None
+threadProc = None
 
 #```````````````````Helper methods````````````````````````````````````````````
 def printTime(): return time.strftime("%m%d:%H%M%S")
@@ -50,106 +28,191 @@ def _printv(msg, level):
 def printv(msg): _printv(msg, 1)
 def printvv(msg): _printv(msg, 2)
 
-from p2plantaccess import Access as pa
-pa.init(); pa.start()
-
-info = pa.request(["info", ["*"]])['*']
-print(f'Attached P2Plant hosts the following PVs: {list(info.keys())}')
-
-#``````````````````Definition of PVs``````````````````````````````````````````
-#typeCode = {
-#'F64':'d',  'F32':'f',  'I64':'l',  'I8':'b',   'U8':'B',   'I16':'h',
-#'U16':'H',  'I32':'i',  'U32':'I',  'I64':'l',
-#}
 typeCode = {#'F64':'d',  'F32':'f',  
-'int64':'l',  'int8':'b',   'uint8':'B',   'char':'s', 'int16':'h',
+'int64':'l',  'int8':'b',   'uint8':'B', 'char*':'s', 'int16':'h',
 'uint16':'H',  'int32':'i',  'uint32':'I',  'int64':'l',
 }
 
-def makeNTScalar(t:str):
-    tt = t.split('*')# vector type ends with *
-    prefix = '' if len(tt)==1 else 'a'
-    return NTScalar(prefix+typeCode[tt[0]], display=True)
+def set_run(vntenum):
+    idx = vntenum.raw.value.index
+    #print(f">set_run {idx}")
+    if idx == 0:# Run
+        threading.Thread(target=threadProc).start()
+
+def makeNTScalar(key:str):
+    prefix = ''
+    if key != 'char*':
+        if key[-1] == '*':
+            key = key[:-1]
+            prefix = 'a'
+    return NTScalar(prefix+typeCode[key], display=True)
+
+PVs = {}# Map of process variables
 
 PVDefs = [# Standard PVs
 ['Run', 'Start/Stop the device',
     NTEnum(),#DNW:display=True),
     {'choices': ['Run','Stop'], 'index': 0},'WE',
-    {'setter': G.set_run}],
+    {'setter': set_run}],
 ['cycle',   'Cycle number', makeNTScalar('uint32'), '0', 'R',{}],
 ]
-for pvName,inf in info.items():
-    printv(f'PV {pvName}: {inf}')
-    r = pa.request(['get',[pvName]])[pvName]
-    printv(f'r: {r}')
-    shape = r.get('shape',[1])
-    if len(shape) > 1:  continue# skip multi-dimensional arrays for now
-    #if inf['type'] in ('char*'): continue#  skip for now
-    PVDefs.append([pvName, inf['desc'], makeNTScalar(inf['type']),
-        r['v'], inf['fbits'], {}])
 
-ts = time.time()
+def append_PVDefs(info:dict):
+    for pvName,inf in info.items():
+        printv(f'PV {pvName}: {inf}')
+        r = PA.request(['get',[pvName]])[pvName]
+        printv(f'r: {r}')
+        shape = r.get('shape',[1])
+        if len(shape) > 1:  continue# skip multi-dimensional arrays for now
+        #if inf['type'] in ('char*'): continue#  skip for now
+        PVDefs.append([pvName, inf['desc'], makeNTScalar(inf['type']),
+            r['v'], inf['fbits'], {}])
 
 #``````````````````create_PVs()```````````````````````````````````````````````
-for defs in PVDefs:
-    pname,desc,nt,ivalue,features,extra = defs
-    writable = 'W' in features
-    #print(f'creating pv {pname}, writable: {writable}, initial: {ivalue}, extra: {extra}')
-    pv = SharedPV(nt=nt)
-    G.PVs[P+pname] = pv
-    pv.open(ivalue)
-    #if isinstance(ivalue,dict):# NTEnum
-    if isinstance(nt,NTEnum):# NTEnum
-        pv.post(ivalue, timestamp=ts)
-    else:
-        v = pv._wrap(ivalue, timestamp=ts)
-        #if display:
-        displayFields = {'display.description':desc}
-        for field in ['limitLow','limitHigh','format','units']:
-            try:    displayFields[f'display.{field}'] = extra[field]
-            except: pass
-        for key,value in displayFields.items():
-            #print(f'Trying to add {key} to {pname}')
-            try:    v[key] = value
-            except Exception as e:
-                printe(f'in adding {key} to {pname}: {e}')
-                pass
-        pv.post(v)
-    pv.name = pname
-    pv.setter = extra.get('setter')
+def create_PVs():
+    tstamp = time.time()
+    for defs in PVDefs:
+        pname,desc,nt,ivalue,features,extra = defs
+        writable = 'W' in features
+        #print(f'creating pv {pname}, writable: {writable}, initial: {ivalue}, extra: {extra}')
+        pv = SharedPV(nt=nt)
+        PVs[pargs.prefix+pname] = pv
+        #print(f'ivalue: {ivalue}')
+        pv.open(ivalue)
+        #if isinstance(ivalue,dict):# NTEnum
+        if isinstance(nt,NTEnum):# NTEnum
+            pv.post(ivalue, timestamp=tstamp)
+        else:
+            v = pv._wrap(ivalue, timestamp=tstamp)
+            #if display:
+            displayFields = {'display.description':desc}
+            for field in ['limitLow','limitHigh','format','units']:
+                try:    displayFields[f'display.{field}'] = extra[field]
+                except: pass
+            for key,value in displayFields.items():
+                #print(f'Trying to add {key} to {pname}')
+                try:    v[key] = value
+                except Exception as e:
+                    printe(f'in adding {key} to {pname}: {e}')
+                    pass
+            pv.post(v)
+        pv.name = pname
+        pv.setter = extra.get('setter')
 
-    if writable:
-        @pv.put
-        def handle(pv, op):
-            ct = time.time()
-            v = op.value()
-            vr = v.raw.value
-            if isinstance(v, ntenum):
-                vr = v
-            if pv.setter:
-                pv.setter(vr)
-            if pargs.verbose >= 1:
-                printi(f'putting {pv.name} = {vr}')
-            pv.post(vr, timestamp=ct) # update subscribers
-            op.done()
+        if writable:
+            @pv.put
+            def handle(pv, op):
+                ct = time.time()
+                v = op.value()
+                vr = v.raw.value
+                if isinstance(v, ntenum):
+                    vr = v
+                if pv.setter:
+                    pv.setter(vr)
+                if pargs.verbose >= 1:
+                    printi(f'putting {pv.name} = {vr}')
+                pv.post(vr, timestamp=ct) # update subscribers
+                op.done()
 #,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
-if pargs.listPVs:
-    print('List of PVs:')
-    pprint.pp(list(G.PVs.keys()))
+lasttime = time.time()
+fps = 0
+def receive_subscribtion(blocking=False):
+    global lasttime,fps
+    ct = time.time()
+    printInterval = 10#s
+    if ct - lasttime >= printInterval:
+        lasttime = ct
+        txt = ''
+        if pargs.keep_alive:
+            # request something to inform server that I am alive
+            r = PA.request('["get", ["perf"]]');
+            txt += f'Perf: {str(r["perf"]["v"])}.'
+        txt += f' Frame rate: {round(fps/printInterval,3)} Hz.'
+        if not pargs.quiet: 
+            printi(txt)
+        fps = 0
+
+    r = PA.recv('subscription', blocking)
+    if len(r) == 0:
+        #printv(f'no data, fps: {fps}')
+        time.decoded(.000001)#ISSUE: if return without any system call, then the cycle rate is slow, same as with -g and CPU=100%. Is this QT issue? With this microsleep the CPU=26% and trig rate=frame. 
+        return {}
+
+    # data received
+    fps += 1
+    printv(f'data received, fps: {fps}')
+    decoded = PA.decode()
+    if pargs.verbose >= 2:
+        printv(f'decoded: {decoded}')
+    return decoded
 
 def myThread_proc():
     threads = 0
+    cycle = 0
     printi('Run started')
+    print(f'pargs: {pargs}')
     while not EventExit.is_set():
-        G.cycle += 1
-        printv(f'cycle {G.cycle}')
-        if str(G.PVs[P+'Run'].current())!='Run':
+        cycle += 1
+        printv(f'cycle {cycle}')
+        if str(PVs[pargs.prefix+'Run'].current())!='Run':
             break
-        EventExit.wait(SleepTime)
+        ts = time.time()
+        PVs[pargs.prefix+'cycle'].post(cycle, timestamp=ts)
+
+        r = receive_subscribtion()
+        for pvname,rd in r.items():
+            printv(f'received {pvname}')
+            #TODO: handle shape
+            if len(rd['shape']) > 1:  continue# skip multi-dimensional arrays for now
+            PVs[pargs.prefix+pvname].post(rd['v'], timestamp=rd['t'])
+
+        EventExit.wait(pargs.sleep)
     printi('Run stopped')
     return
 
-G.threadProc = myThread_proc
-thread = threading.Thread(target=myThread_proc).start()
+#``````````````````Main function``````````````````````````````````````````````
+def main():
+    import argparse
+    global pargs, threadProc
+    # Argument parsing
+    parser = argparse.ArgumentParser(description = __doc__,
+      formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+      epilog=(f'{__version__}'))
+    parser.add_argument('-k', '--keep_alive', action='store_true', help=\
+      'Periodically send request to server to keep it alive')
+    parser.add_argument('-l', '--listPVs', action='store_true', help=\
+    'List all generated PVs')
+    parser.add_argument('-p', '--prefix', default='p2p:', help=\
+    'Prefix of all PVs')
+    parser.add_argument('-s','--sleep',type=float,default=.1, help=\
+    'Sleep time between checking for subscription delivery')
+    parser.add_argument('-q', '--quiet', action='store_true', help=\
+      'Do not print frame statistics')
+    parser.add_argument('-v', '--verbose', action='count', default=0, help=\
+    'Show more log messages (-vv: show even more)')
+    pargs = parser.parse_args()
 
-Server.forever(providers=[G.PVs]) # runs until KeyboardInterrupt
+    # Connect to plant and start it.
+    PA.init();
+    PA.start()
+    r = PA.request('["get", ["version"]]');
+    print(f'P2Plant {r["version"]["v"]}')
+    info = PA.request(["info", ["*"]])['*']
+    print(f'Attached P2Plant hosts the following PVs: {list(info.keys())}')
+    PA.request('["set", [["run", "start"]]]')
+
+    # Construct PVs
+    append_PVDefs(info)
+    create_PVs()
+    if pargs.listPVs:
+        print('List of PVs:')
+        pprint.pp(list(PVs.keys()))
+
+    #```````````````The P2Plant seems to be alive`````````````````````````````````
+    threadProc = myThread_proc
+    thread = threading.Thread(target=myThread_proc).start()
+
+    Server.forever(providers=[PVs]) # runs until KeyboardInterrupt
+
+if __name__ == "__main__":
+    main()
